cc8b1fecba153cd26384e4e7aa1b0e42a038b950
Adan Silva <adan.t.silva77@gmail.com>
2022-10-05 15:29:04 -0700

    Move around CTREConfigs class

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.lib.config.CTREConfigs;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
