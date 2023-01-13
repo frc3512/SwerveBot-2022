f7c1fbe6762d7223421119fccfea28bfb0bfd2d0
Murad <agariomadman@gmail.com>
2023-01-13 15:57:52 -0600

    Create AutoBalancing.java

---
+++ src/main/java/frc/robot/commands/AutoBalancing.java
@@ -0,0 +1,27 @@
+package frc.robot.commands;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.Swerve;
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+
+public class AutoBalancing extends CommandBase {
+  private Swerve s_Swerve;
+  public AutoBalancing(Swerve s_Swerve){
+  this.s_Swerve = s_Swerve;
+    addRequirements(s_Swerve);
+
+    this.translationSup = translationSup;
+    this.strafeSup = strafeSup;
+    this.rotationSup = rotationSup;
+    this.robotCentricSup = robotCentricSup;
+  }
+
+  @Override
+  public void execute() {
+  }
+}
