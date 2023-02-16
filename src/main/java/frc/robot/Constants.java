c8a23ec38f36d0405b051ffa8416f4e4a47388da
ZaraSofia <112798040+ZaraSofia@users.noreply.github.com>
2023-02-16 15:19:07 -0600

    Update Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -205,5 +205,16 @@ public final class Constants {
 
     public static final double maxCurrentIntake = 80; 
 
+    //wheel diameter, gear ratio, encoder constants
+        //will need to change depending on the robot/swerve
+        public static final double kWheelDiameterMeters = Units.inchesToMeters(4);
+        public static final double kDriveMotorGearRatio = 1 / 5.8462;
+        public static final double kTurningMotorGearRatio = 1 / 18.0;
+        public static final double kDriveEncoderRot2Meter = kDriveMotorGearRatio * Math.PI * kWheelDiameterMeters;
+        public static final double kTurningEncoderRot2Rad = kTurningMotorGearRatio * 2 * Math.PI;
+        public static final double kDriveEncoderRPM2MeterPerSec = kDriveEncoderRot2Meter / 60;
+        public static final double kTurningEncoderRPM2RadPerSec = kTurningEncoderRot2Rad / 60;
+        public static final double kPTurning = 0.5;
+
   }
 }
